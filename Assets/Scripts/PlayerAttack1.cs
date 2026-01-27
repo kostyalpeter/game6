e524143cb6ee2faa0151fb291bc50d9a63d5e034
kostyálpéter <kostyal.peter123@gmail.com>
2026-01-27 20:41:26 +0100

    new scenes, character chooser selecter in progress

--- Assets/Scripts/PlayerAttack1.cs
+++ Assets/Scripts/PlayerAttack1.cs
@@ -1,5 +1,3 @@
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
---
+++ Assets/Scripts/PlayerChoose.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+
+public class PlayerChoose : MonoBehaviour
+{
+    public TMP_Text Health;
+
+
+    public void Player1()
+    {
+    }
+}
